f4c0dfbcec2083572c9513915477b2a19bcc21e7
Per Jansson <per.jansson@hbo.com>
2018-10-01 14:34:08 +0300

    Added mock data

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,6 +4,24 @@ import { storiesOf, addDecorator, configure } from '@storybook/react'
 import { DataEntitiesProvider } from '../DataEntitiesContext'
 import DataEntityComponent from '../DataEntityComponent'
 
+import { addDataEntities } from '../util/dataEntityStore'
+import mockLinks from './mock/links.json'
+import mockLinkset from './mock/linkset.json'
+import mockGrid from './mock/grid.json'
+import mockTrays from './mock/trays.json'
+import mockPage from './mock/page.json'
+import mockText from './mock/text.json'
+
+// Initialize with mocked data entities from backend
+const dataEntities = addDataEntities(
+  mockLinks,
+  mockLinkset,
+  mockGrid,
+  mockTrays,
+  mockPage,
+  mockText,
+)
+
 const WithContext = story => (
   <DataEntitiesProvider>{story()}</DataEntitiesProvider>
 )
--- src/util/dataEntityStore.js
+++ src/util/dataEntityStore.js
@@ -1,100 +1,25 @@
-const dataEntities = {
-  'urn:hbo:page:series-family': {
-    body: {
-      references: {
-        items: [
-          'urn:hbo:linkset:series',
-          'urn:hbo:text:series-family-title',
-          'urn:hbo:carousel:series-family-featured',
-          'urn:hbo:tray:series-family-collection1',
-          'urn:hbo:grid:series-family-a-z',
-          'urn:hbo:tray:series-family-collection2',
-        ],
-      },
-    },
-  },
+import { is, omit } from 'ramda'
 
-  'urn:hbo:linkset:series': {
-    body: {
-      references: {
-        links: [
-          'urn:hbo:link:series-all',
-          'urn:hbo:link:series-latino',
-          'urn:hbo:link:series-international',
-          'urn:hbo:link:series-family',
-        ],
-      },
-    },
-  },
+let dataEntities = {}
 
-  'urn:hbo:link:series-all': {
-    body: {
-      label: 'All',
-      references: {
-        target: 'urn:hbo:page:series-all',
-      },
-    },
-  },
-  'urn:hbo:link:series-latino': {
-    body: {
-      label: 'Latino',
-      references: {
-        target: 'urn:hbo:page:series-latino',
-      },
-    },
-  },
-  'urn:hbo:link:series-international': {
-    body: {
-      label: 'International',
-      references: {
-        target: 'urn:hbo:page:series-international',
-      },
-    },
-  },
-  'urn:hbo:link:series-family': {
-    body: {
-      label: 'Family',
-      references: {
-        target: 'urn:hbo:page:series-family',
-      },
-    },
-  },
+const updateDataEntities = (dataEntities, ...newDataEntities) => ({
+  ...dataEntities,
+  ...newDataEntities.reduce(
+    (memo, dataEntity) => ({
+      ...memo,
+      ...(is(Array, dataEntity)
+        ? {
+            ...memo,
+            ...updateDataEntities(dataEntities, ...dataEntity),
+          }
+        : { ...memo, [dataEntity.id]: dataEntity }),
+    }),
+    {},
+  ),
+})
 
-  'urn:hbo:text:series-family-title': {
-    body: {
-      value: 'Family Series',
-    },
-  },
-
-  'urn:hbo:tray:series-family-collection1': {
-    body: {
-      style: 'STANDARD',
-      references: {
-        items: [],
-        target: 'urn:hbo:page:series-family-all',
-      },
-    },
-  },
-  'urn:hbo:tray:series-family-collection2': {
-    body: {
-      style: 'FANCY',
-      references: {
-        items: [],
-        target: 'urn:hbo:page:series-family-all',
-      },
-    },
-  },
-
-  'urn:hbo:grid:series-family-a-z': {
-    body: {
-      header: {
-        label: 'All Family Series',
-      },
-      references: {
-        items: [],
-      },
-    },
-  },
+export const addDataEntities = (...newDataEntities) => {
+  dataEntities = updateDataEntities(dataEntities, ...newDataEntities)
 }
 
 export default id => (dataEntities[id] ? dataEntities[id].body : undefined)
